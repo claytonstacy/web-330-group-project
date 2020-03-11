5bf3eab4261d6f8beffaa703dcb5b84a46ec12e4
Clayton Stacy <clayton.stacy@gmail.com>
2020-03-11 14:04:10 -0500

    Adding in View Model and card flip animation

--- javascript.js
+++ javascript.js
@@ -1,24 +1,4 @@
-// set slideshow at 1
-var slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-  };
-  
-  // Question slide
-  function currentSlide(n) {
-    showSlides(slideIndex = n);
-  };
-
-  function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("slide");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    };
-    slides[slideIndex-1].style.display = "flex";
-  };
+function flipper() {
+    var element = document.getElementById("cardID");
+    element.classList.toggle("flip");
+  }
