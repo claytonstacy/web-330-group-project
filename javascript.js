38a2ebbef37cd784803b37319068482e2815c899
i-kornelis <izabellakornelis@gmail.com>
2020-03-29 13:59:45 -0700

    show feedback based on selected answer

--- javascript.js
+++ javascript.js
@@ -2,3 +2,32 @@ function flipper() {
     var element = document.getElementById("cardID");
     element.classList.toggle("flip");
   }
+
+/* Button Colors
+
+let answerButtons = document.getElementsByClassName('button');
+
+answerButtons.addEventListener('click', selectAnswer);
+
+function selectAnswer(e) {
+  const selectedAnswer = e.target
+  const correct = self.activeQuestion().rightAnswer
+
+  Array.from(answerButtons.children).forEach(button => {
+    setStatusClass(button, button.dataset.correct)
+  })
+}
+function setStatusClass(element, correct) {
+  clearStatusClass(element)
+  if (correct) {
+    element.classList.add('correct')
+  } else {
+    element.classList.add('wrong')
+  }
+}
+
+function clearStatusClass(element) {
+  element.classList.remove('correct')
+  element.classList.remove('wrong')
+}
+*/
